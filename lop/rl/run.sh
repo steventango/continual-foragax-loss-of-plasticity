316c4cde518626a925e82121fa3b6099edc8affb
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-17 23:00:17 -0600

    Update script

--- lop/rl/run.sh
+++ lop/rl/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
 
-parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/cbp.yml -s {1} ::: $(seq 20 24) &
-parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/std.yml -s {1} ::: $(seq 20 24) &
+parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/cbp/{1}.yml -s {1} ::: $(seq 0 29) &
+wait
+parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/std/{1}.yml -s {1} ::: $(seq 0 29) &
+wait
