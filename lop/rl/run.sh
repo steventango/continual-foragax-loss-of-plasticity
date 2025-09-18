a08c7f0f2b229b5d147c406b13c418ea9b25fb43
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-18 00:08:19 -0600

    Rename configs

--- lop/rl/run.sh
+++ lop/rl/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 clear
 
-parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/cbp/{1}.yml -s {1} ::: $(seq 0 29) &
-wait
-parallel --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/std/{1}.yml -s {1} ::: $(seq 0 29) &
-wait
+parallel -j 15 --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/PPO_CB/{1}.yml -s {1} ::: $(seq 0 29)
+parallel -j 15 --eta --ungroup taskset -c {1} python3 run_ppo.py -c cfg/foragax/ForagaxTwoBiomeSmall-v2-15/PPO/{1}.yml -s {1} ::: $(seq 0 29)
